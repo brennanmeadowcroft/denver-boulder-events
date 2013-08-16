c0607d3a4cbfa2f2c4a6f460f09c98239609e76f
Brennan Meadowcroft <brennan@brennanmeadowcroft.com>
2013-08-15 23:03:50 -0600

    Add recurrence to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,44 @@ class Event < ActiveRecord::Base
 
   has_and_belongs_to_many :tags
 
-  def generate_ics_file
+  def recurrence_rule
+    if !self.frequency.nil? && self.frequency != ''
+      freq = "RRULE:FREQ=#{self.frequency.upcase};"
+    else
+      freq = ''
+    end
+
+    if !self.recurrence_end_date.nil? && self.recurrence_end_date != ''
+      until_date = "UNTIL=#{self.recurrence_end_date.to_s(:long_ical)};"
+    else
+      until_date = ''
+    end
+
+    if !self.recurrence_frequency.nil? && self.recurrence_frequency != ''
+      interval = "INTERVAL=#{self.recurrence_frequency};"
+    else
+      interval = ''
+    end
+
+    if !self.recurrence_day.nil? && self.recurrence_day != ''
+      weekday = "BYDAY=#{self.recurrence_day.upcase.gsub(/\s/, '')};"
+    else
+      weekday = ''
+    end
+
+    if !self.recurrence_end_count.nil? && self.recurrence_end_count != ''
+      if weekday != ''
+        total_times = self.recurrence_day.split(/,\s/).count
+      else
+        total_times = 1
+      end
+      n_times = "COUNT=#{self.recurrence_end_count * total_times};"
+    else 
+      n_times = ''
+    end
+
+    rrule = freq + n_times + until_date + interval + weekday
+    rrule.chomp(";")
   end
 
   def tag_tokens=(tokens)
